5165c27b6c7d4ea5e7b92221eea7dee2301bbcc8
Veronika Mackevičiūtė <117922006+VeronikaMack@users.noreply.github.com>
2023-03-02 20:03:51 +0200

    Add files via upload

---
+++ mylib.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <fstream>
+#include <random>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::copy;
+using std::numeric_limits;
+using std::vector;
+using std::mt19937;
+
+
