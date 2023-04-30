8b7d1392ac98168766f8e88d230bd19255b0652f
Veronika Mackevičiūtė <117922006+VeronikaMack@users.noreply.github.com>
2023-04-30 15:43:05 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -2,7 +2,6 @@
 
 class studentas
 {
-    // realizacija
 private:
     std::string vardas;
     std::string pavarde;
@@ -10,26 +9,23 @@ private:
     std::vector<int> paz;
     double vidurkis;
     double mediana;
-    // interfeisas
 public:
-    studentas() : vardas(""), pavarde(""), egz(0), paz(0), vidurkis(0.0), mediana(0.0) {} // default konstruktorius
-    void Vardas(const string&v){vardas=v;}//setteris
-    void Pavarde(const string&p){pavarde=p;}//seteris
-    void Egz(const int&e){egz = e;}//setters
-    void Paz(const vector<int>&nd){paz = nd;}//seteris
+    studentas() : vardas(""), pavarde(""), egz(0), paz(0), vidurkis(0.0), mediana(0.0) {} 
+    void Vardas(const string&v){vardas=v;}
+    void Pavarde(const string&p){pavarde=p;}
+    void Egz(const int&e){egz = e;}
+    void Paz(const vector<int>&nd){paz = nd;}
     void failas(std::istream& is, int k);
     void Vid(const double&vid){vidurkis = vid;}
     void Med(const double&med){mediana = med;}
-    //spaudinimo funk
-    //balo func
     void clear() {paz.clear(), paz.shrink_to_fit();}
-    inline std::string getvardas() const { return vardas; }       // get'eriai, inline
-    inline std::string getpavarde() const { return pavarde; }     // get'eriai, inline
+    inline std::string getvardas() const { return vardas; }       
+    inline std::string getpavarde() const { return pavarde; }     
     inline double getmediana() const {return mediana;}
-    inline double getvidurkis() const {return vidurkis;} // get'eriai
-    inline vector<int> getpaz() const {return paz;}//geteris
-    inline double getegz()const {return egz;}//getteris
-    ~studentas() {paz.clear(), paz.shrink_to_fit();} //destruct                
+    inline double getvidurkis() const {return vidurkis;} 
+    inline vector<int> getpaz() const {return paz;}
+    inline double getegz()const {return egz;}
+    ~studentas() {paz.clear(), paz.shrink_to_fit();}            
 };
 
 void pild(studentas &temp);
